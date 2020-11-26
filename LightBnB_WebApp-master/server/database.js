8a3f72f905ad59ec26fd5e72488bc3fe51cc29de
Dante Cross <dantecross@outlook.com>
2020-11-26 20:22:48 +0000

    added sql query for my reservtions to database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -82,7 +82,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = (`
+  SELECT reservations.*, properties.*, AVG(rating) as average_rating
+FROM reservations
+JOIN users ON users.id = guest_id
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = $1
+AND reservations.end_date < NOW()::DATE
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+  `)
+  const values = [guest_id, limit]
+  return pool.query(query, values)
+    .then((res) => res.rows)
+    .catch(err => console.error('error', err.stack))
 }
 exports.getAllReservations = getAllReservations;
 
